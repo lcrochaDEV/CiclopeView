acfe9f07616e07c27d5fd626d3d738640ddce44c
Lucas Rocha <lucas@Servidor-LNX.meuintelbras.local>
2024-01-20 11:31:59 -0300

    Instalação Ambiente env

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
  ## Desenvolvido em Python3.11.2 <img src="./imagens/olho.png" width="50px" margin="0" padding="0">
 
+
+ ### Para Instalação no Debia 12 foi criado um Ambiente Virtual
+ #### Crie um ambiente virtual usando venv ou virtual env Certifique-se venv de que esteja instalado executando:
+```shell
+sudo apt install python3-venv
+```
+#### Para criar um novo ambiente virtual em um **diretório chamado env**, execute:
+```shell
+python3 -m venv env
+```
+#### Para ativar este ambiente virtual (que modifica a PATH variável de ambiente), execute:
+```shell
+source env/bin/activate
+```
+#### Agora você pode instalar uma biblioteca neste ambiente virtual:
+```shell
+pip install XYZ
+```
+Os arquivos serão instalados no env/diretório.
+
+Se quiser sair do ambiente virtual, você pode executar:
+```shell
+deactivate
+```
+Retirado do Post [askubuntu](https://askubuntu.com/questions/1465218/pip-error-on-ubuntu-externally-managed-environment-%C3%97-this-environment-is-extern)
+
+<font>Lembre de criair o arquivo .gitignore para evitar que essa pasta suba para o github</font>
+
 ### Instalação de Bibliotecas:
 
- * Biblioteca [netmiko](https://pypi.org/project/netmiko/), apenas na versão 2.4.2 e não está integrado o pacote [ntc-templates](https://pypi.org/project/ntc-templates/4.0.1/) nas versão do Debian, na versão Windows o pacote é mais completo, e vem com o ntc-templates default.
+ * netmiko
+ * dotenv
+ * json
+ * os
+ * re
+ * time
+
+
 
 ```shell
 #sudo apt install python3-netmiko
@@ -25,13 +60,14 @@
 #sudo apt-get purge python3-netmiko 
 ```
 
-### Lista de Pacotes Instalados no Windows
+### Lista de Pacotes Instalados no Windows/Linux
 
 ```shell
 #netmiko          4.3.0
 #ntc_templates    4.1.0
 #paramiko         3.4.0
 ```
+
 
 ![netmiko](https://i0.wp.com/networkautomationlane.in/wp-content/uploads/2021/08/netmiko.png?fit=640%2C244&ssl=1)
 
--- exec.py
+++ exec.py
@@ -29,8 +29,10 @@
 from ControllerClass import ClassConsole
 from ControllerClass import ClassCommand
 import re
+import time
+#import keyboard
 
-
+# ÁREA DE AUTENTICAÇÃO
 #ipEndHop = "10.18.228.193"
 #ip_Destination = "10.18.228.196"
 device_type = "cisco_ios_telnet"
@@ -44,8 +46,13 @@ port = "41479"
 #ip_Destination = input("IP Destination: ")
 #port = input("Port: ")
 #device_type = input("Device: ")
-commandConsole = input("Command: ")
+#commandConsole = input("Command: ")
 
+
+for i in range(4):
+    time.sleep(1)
+    print(f"Processando", end="")
+    print("." *i)
 
 def validationText(text):
     if re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\w[a-z]+\d{2}\.\w+|\w+\.\d{2}|\w+\.\w+.com.\w+", text):
@@ -56,13 +63,14 @@ def validationText(text):
 validationText(ipEndHop)  
 
 #INICIO DO CONSOLE
+'''
 def send_show_console_init():
     ios = ClassConsole.CommandConsole(f"{device_type}", f"{ipEndHop}", f"{commandConsole}", f"{port}")
     printInit = ios.commandInit()
     print("-"*100)
     print(f'{printInit}')
 #send_show_console_init()
-
+'''
 #INICIO DO PROGRAMA
 def send_show_command_init():
     command = ["sh ver | s uptime", "sh clock", "show ip int br", "sh ver"]
@@ -71,46 +79,29 @@ def send_show_command_init():
         printInit = ios.commandInit()
         print("-"*100)
         print(f'{printInit}')
-send_show_command_init()
+#send_show_command_init()
 
 #FUNÇÃO PROCURA SUB-INTERFACE
 
-'''
-command = f"show ip route {ip_Destination} | i directly connected, via"
-generic = ClassCommand.ControllerConnect(f"{device_type}", f"{ipEndHop}", f"{command}", f"{port}")
-endInt = generic.commandInit()
-if endInt:
-    sub_interface = re.findall(r"\w+?[\/\d].*", endInt)[0]
-    interface = re.findall(r"\w+\w[\/\d]+|\w+\s\d+", endInt)[0]
+commands = [
+    #"sh run | i hostname",
+    #f"show int {interface} | i line protocol", 
+    #f"show int {interface} | i  Hardware",
+    #"show processes | i CPU",
+    #"sh ver | s uptime", #
+    #"sh clock", #
+    #f"show ip int br" #
+    "sh ver" #
+    ]
 
-if endInt:
-    commands = [
-        #"sh run | i hostname",
-        #f"show ip route {ip_Destination} | i directly connected, via", 
-        #f"show int {interface} | i line protocol", 
-        #f"show int {interface} | i  Hardware",
-        #f"show running-config interface {sub_interface} | i description",
-        #"show processes | i CPU",
-        #f"show arp {sub_interface}",
-        #f"ping {ip_Destination}"
-        #"sh ver | s uptime", #
-        #"sh clock", #
-        #f"show ip int br" #
-        #"sh ver" #
-        #f"show int {interface}" #
-        ]
-'''
+def send_show_command(command):
+    for command in commands:
+        ios = ClassCommand.ControllerConnect(f"{device_type}", f"{ipEndHop}", f"{command}", f"{port}")
+        cliReturn = ios.commandOne()
+        print("-"*100)
+        print(f'{cliReturn}')
 
-'''
-    def send_show_command(command):
-        for command in commands:
-            ios = ClassCommand.ControllerConnect(f"{device_type}", f"{ipEndHop}", f"{command}", f"{port}")
-            #cliReturn = ios.commandExecJson()
-            cliReturn = ios.commandOne()
-            print("-"*100)
-            print(f'{cliReturn}')
-
-    send_show_command(commands)
+send_show_command(commands)
 
 
 def send_show_command_All():
@@ -119,10 +110,7 @@ def send_show_command_All():
         ios = ClassCommand.ControllerConnect(f"{device_type}", f"{ipEndHop}", f"{commands}", f"{port}")
         printInit = ios.commandFilterAll()
         print("-"*100)
-        #print(f'{printInit}')
 
         for interface in printInit:
             print(f"{interface['interface']}")
-'''
-
 #send_show_command_All()
